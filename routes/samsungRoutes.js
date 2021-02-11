ec78f5a75e8c6ae5ba0e36e5e032e008dd03b537
Nathanael Hauser <nathanaelhauser@gmail.com>
2021-02-10 21:23:32 -0800

    Added pause button to remote

--- routes/samsungRoutes.js
+++ routes/samsungRoutes.js
@@ -69,6 +69,9 @@ module.exports = app => {
           case "play":
             key = KEYS.KEY_PLAY;
             break;
+          case "pause":
+            key = KEYS.KEY_PAUSE;
+            break;
           default:
             key = null;
             break;
